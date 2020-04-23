aeb58ca242dfb0fdcc814c1bc9198a033887dddb
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-23 23:58:05 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,12 @@
+class Shoe
+  attr_accessor :brand,:size,:color,:material,:condition
+  def initialize(brand)
+    @brand=brand
+  end
+
+  def cobble(condition="new")
+    @condition=condition
+    puts "Your shoe is as good as new!"
+  end
+
+end
